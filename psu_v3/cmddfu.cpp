b4caa154479ed219419df7944af893b474a26f8e
hreis <a38652@alunos.isel.pt>
2019-09-23 22:34:45 +0100

    Added macros

--- psu_v3/cmddfu.cpp
+++ psu_v3/cmddfu.cpp
@@ -1,20 +1,18 @@
 #include "cmddfu.h"
 #include "board.h"
 
-extern uint32_t _estack;
-
 void CmdDfu::help(void){ 
 //console->print("Adc Command\n");
 }
 
 char CmdDfu::execute(void *ptr){
-uint64_t *dfu_magic = (uint64_t*)&_estack;
+
 
     console->print("Entering DFU mode\n");
     LCD_ClrArea(0, 0, LCD_W, LCD_H);
     LCD_Update();
 
-	*(dfu_magic - 1) = 0xDEADBEEFCC00FFEEULL;
+	ENABLE_DFU_MAGIC;
     NVIC_SystemReset();
     
     return CMD_OK; 
--- psu_v3/cmddfu.h
+++ psu_v3/cmddfu.h
@@ -8,10 +8,15 @@ extern "C" {
 
 #include <console.h>
 
+#define ENABLE_DFU_MAGIC *(((uint64_t*)&_estack) - 1) = 0xDEADBEEFCC00FFEEULL
+#define DISABLE_DFU_MAGIC *(((uint64_t*)&_estack) - 1) = 0x0UL
+
+extern uint32_t _estack;
+
 class CmdDfu : public ConsoleCommand{
     Console *console;
 public:
-    void init(void *params) { console = static_cast<Console*>(params); }
+    void init(void *params) { DISABLE_DFU_MAGIC; console = static_cast<Console*>(params); }
     CmdDfu () : ConsoleCommand("dfu") { }
     char execute(void *ptr);
     void help(void);
