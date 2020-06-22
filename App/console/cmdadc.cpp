2ac5f5b16e45fb1016af2998697f022c523a12af
r315 <a38652@alunos.isel.pt>
2020-06-22 22:23:25 +0100

    wip: adc command

--- App/console/cmdadc.cpp
+++ App/console/cmdadc.cpp
@@ -1,21 +1,39 @@
 #include "cmdadc.h"
 #include "board.h"
 
+char *getOptValue(const char *opt, uint32_t argc, char **argv);
+
 void CmdAdc::help(void){ 
-console->print("Usage: adc\n");
-console->print("Command not implemented\n");
+    console->print("Usage: adc <command>\n");
+    console->print("Adc configuration\n\n");
+    console->print("Commands:\n");
+    console->print("\tres\t\tResolution\n");
+    console->print("\tcal\t\tCalibration data\n");
 }
 
 char CmdAdc::execute(void *ptr){ 
-/* uint16_t *buf;
-    do{
-        console->print("\n");
-        buf = ADC_LastConvertion();
-        for(uint8_t i = 0; i < 4; i++){
-            console->print("ADC[%d] = %u \n", i, buf[i]);
-        }      
-        DelayMs(400);
-    }while(!console->kbhit());
- */   
-    return CMD_OK; 
+char *argv[4], *param;
+uint32_t argc;
+uint16_t value;
+    
+    argc = strToArray((char*)ptr, argv);
+
+    if(argc == 0){
+        help();
+        return CMD_OK;
+    }
+
+    if((param = getOptValue("res", argc, argv)) != NULL){
+        float res = ADC_GetResolution();
+        console->print("Resolution: %.2fmv\n", res);
+        return CMD_OK;
+    }
+
+    if((param = getOptValue("cal", argc, argv)) != NULL){
+        uint16_t res = ADC_GetCalibration();
+        console->print("Calibration: %u\n", res);
+        return CMD_OK;
+    }
+
+    return CMD_BAD_PARAM;
 }
